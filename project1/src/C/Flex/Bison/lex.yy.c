ade42d9a2c3564a153c75ab2db53ec50837f26fd
Alan Ai <aijunda29@gmail.com>
2020-09-20 12:12:04 +0800

    Changed char value type regex

--- project1/src/C/Flex/Bison/lex.yy.c
+++ project1/src/C/Flex/Bison/lex.yy.c
@@ -370,8 +370,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 37
-#define YY_END_OF_BUFFER 38
+#define YY_NUM_RULES 38
+#define YY_END_OF_BUFFER 39
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -381,15 +381,15 @@ struct yy_trans_info
 	};
 static const flex_int16_t yy_accept[85] =
     {   0,
-        0,    0,   38,   36,    1,    1,   12,   36,   36,   13,
-       14,   10,    8,    4,    9,    2,   11,   31,   31,    3,
-        6,    5,    7,   35,   15,   16,   35,   35,   35,   35,
-       35,   35,   35,   17,   36,   18,   21,   23,    0,    0,
-        0,   31,   31,   25,    0,   31,    0,   19,   22,   20,
-       35,   35,   35,   35,   27,   35,   35,   35,   35,   24,
-       33,    0,   32,   31,   35,   35,   35,   34,   35,   35,
-       35,    0,   31,   28,   35,   35,   35,   35,   35,   35,
-       29,   30,   26,    0
+        0,    0,   39,   37,    1,    2,   13,   37,   37,   14,
+       15,   11,    9,    5,   10,    3,   12,   32,   32,    4,
+        7,    6,    8,   36,   16,   17,   36,   36,   36,   36,
+       36,   36,   36,   18,   37,   19,   22,   24,    0,    0,
+        0,   32,   32,   26,    0,   32,    0,   20,   23,   21,
+       36,   36,   36,   36,   28,   36,   36,   36,   36,   25,
+       34,    0,   33,   32,   36,   36,   36,   35,   36,   36,
+       36,    0,   32,   29,   36,   36,   36,   36,   36,   36,
+       30,   31,   27,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -506,10 +506,10 @@ static const flex_int16_t yy_chk[172] =
     } ;
 
 /* Table of booleans, true if rule could match eol. */
-static const flex_int32_t yy_rule_can_match_eol[38] =
+static const flex_int32_t yy_rule_can_match_eol[39] =
     {   0,
-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,     };
+0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,     };
 
 static yy_state_type yy_last_accepting_state;
 static char *yy_last_accepting_cpos;
@@ -529,8 +529,17 @@ char *yytext;
 #line 2 "lex.l"
   #include <stdlib.h>
   #include "syntax.tab.h"
-#line 533 "lex.yy.c"
-#line 534 "lex.yy.c"
+
+  int yycolno = 1;
+
+  #define YY_USER_ACTION \
+    yylloc.first_line = yylineno; \
+    yylloc.first_column = yycolno; \
+    yylloc.last_line = yylineno; \
+    yylloc.last_column = yycolno + yyleng; \
+    yycolno += yyleng;
+#line 542 "lex.yy.c"
+#line 543 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -747,10 +756,10 @@ YY_DECL
 		}
 
 	{
-#line 15 "lex.l"
+#line 24 "lex.l"
 
 
-#line 754 "lex.yy.c"
+#line 763 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -818,195 +827,200 @@ do_action:	/* This label is used only to access EOF actions. */
 			goto yy_find_action;
 
 case 1:
-/* rule 1 can match eol */
 YY_RULE_SETUP
-#line 17 "lex.l"
+#line 26 "lex.l"
 { /* whitespace characters, do nothing */ }
 	YY_BREAK
 case 2:
+/* rule 2 can match eol */
 YY_RULE_SETUP
-#line 18 "lex.l"
-{ return DOT; }
+#line 27 "lex.l"
+{ yycolno = 1; }
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 19 "lex.l"
-{ return SEMI; }
+#line 28 "lex.l"
+{ return DOT; }
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
-#line 20 "lex.l"
-{ return COMMA; }
+#line 29 "lex.l"
+{ return SEMI; }
 	YY_BREAK
 case 5:
 YY_RULE_SETUP
-#line 21 "lex.l"
-{ return ASSIGN; }
+#line 30 "lex.l"
+{ return COMMA; }
 	YY_BREAK
 case 6:
 YY_RULE_SETUP
-#line 22 "lex.l"
-{ return LT; }
+#line 31 "lex.l"
+{ return ASSIGN; }
 	YY_BREAK
 case 7:
 YY_RULE_SETUP
-#line 23 "lex.l"
-{ return GT; }
+#line 32 "lex.l"
+{ return LT; }
 	YY_BREAK
 case 8:
 YY_RULE_SETUP
-#line 24 "lex.l"
-{ return PLUS; }
+#line 33 "lex.l"
+{ return GT; }
 	YY_BREAK
 case 9:
 YY_RULE_SETUP
-#line 25 "lex.l"
-{ return MINUS; }
+#line 34 "lex.l"
+{ return PLUS; }
 	YY_BREAK
 case 10:
 YY_RULE_SETUP
-#line 26 "lex.l"
-{ return MUL; }
+#line 35 "lex.l"
+{ return MINUS; }
 	YY_BREAK
 case 11:
 YY_RULE_SETUP
-#line 27 "lex.l"
-{ return DIV; }
+#line 36 "lex.l"
+{ return MUL; }
 	YY_BREAK
 case 12:
 YY_RULE_SETUP
-#line 28 "lex.l"
-{ return NOT; }
+#line 37 "lex.l"
+{ return DIV; }
 	YY_BREAK
 case 13:
 YY_RULE_SETUP
-#line 29 "lex.l"
-{ return LP; }
+#line 38 "lex.l"
+{ return NOT; }
 	YY_BREAK
 case 14:
 YY_RULE_SETUP
-#line 30 "lex.l"
-{ return RP; }
+#line 39 "lex.l"
+{ return LP; }
 	YY_BREAK
 case 15:
 YY_RULE_SETUP
-#line 31 "lex.l"
-{ return LB; }
+#line 40 "lex.l"
+{ return RP; }
 	YY_BREAK
 case 16:
 YY_RULE_SETUP
-#line 32 "lex.l"
-{ return RB; }
+#line 41 "lex.l"
+{ return LB; }
 	YY_BREAK
 case 17:
 YY_RULE_SETUP
-#line 33 "lex.l"
-{ return LC; }
+#line 42 "lex.l"
+{ return RB; }
 	YY_BREAK
 case 18:
 YY_RULE_SETUP
-#line 34 "lex.l"
-{ return RC; }
+#line 43 "lex.l"
+{ return LC; }
 	YY_BREAK
 case 19:
 YY_RULE_SETUP
-#line 35 "lex.l"
-{ return LE; }
+#line 44 "lex.l"
+{ return RC; }
 	YY_BREAK
 case 20:
 YY_RULE_SETUP
-#line 36 "lex.l"
-{ return GE; }
+#line 45 "lex.l"
+{ return LE; }
 	YY_BREAK
 case 21:
 YY_RULE_SETUP
-#line 37 "lex.l"
-{ return NE; }
+#line 46 "lex.l"
+{ return GE; }
 	YY_BREAK
 case 22:
 YY_RULE_SETUP
-#line 38 "lex.l"
-{ return EQ; }
+#line 47 "lex.l"
+{ return NE; }
 	YY_BREAK
 case 23:
 YY_RULE_SETUP
-#line 39 "lex.l"
-{ return AND; }
+#line 48 "lex.l"
+{ return EQ; }
 	YY_BREAK
 case 24:
 YY_RULE_SETUP
-#line 40 "lex.l"
-{ return OR; }
+#line 49 "lex.l"
+{ return AND; }
 	YY_BREAK
 case 25:
 YY_RULE_SETUP
-#line 42 "lex.l"
-{ char c; while ((c=input()) != '\n'); }
+#line 50 "lex.l"
+{ return OR; }
 	YY_BREAK
 case 26:
 YY_RULE_SETUP
-#line 44 "lex.l"
-{ return STRUCT; }
+#line 52 "lex.l"
+{ char c; while ((c=input()) != '\n'); }
 	YY_BREAK
 case 27:
 YY_RULE_SETUP
-#line 45 "lex.l"
-{ return IF; }
+#line 54 "lex.l"
+{ return STRUCT; }
 	YY_BREAK
 case 28:
 YY_RULE_SETUP
-#line 46 "lex.l"
-{ return ELSE; }
+#line 55 "lex.l"
+{ return IF; }
 	YY_BREAK
 case 29:
 YY_RULE_SETUP
-#line 47 "lex.l"
-{ return WHILE; }
+#line 56 "lex.l"
+{ return ELSE; }
 	YY_BREAK
 case 30:
 YY_RULE_SETUP
-#line 48 "lex.l"
-{ return RETURN; }
+#line 57 "lex.l"
+{ return WHILE; }
 	YY_BREAK
 case 31:
 YY_RULE_SETUP
-#line 50 "lex.l"
-{ yylval = atoi(yytext); return INT; }
+#line 58 "lex.l"
+{ return RETURN; }
 	YY_BREAK
 case 32:
 YY_RULE_SETUP
-#line 51 "lex.l"
-{ yylval = atof(yytext); return FLOAT; }
+#line 60 "lex.l"
+{ yylval = atoi(yytext); return INT; }
 	YY_BREAK
 case 33:
 YY_RULE_SETUP
-#line 52 "lex.l"
-{ yylval = yytext; return CHAR; }
+#line 61 "lex.l"
+{ yylval = atof(yytext); return FLOAT; }
 	YY_BREAK
 case 34:
 YY_RULE_SETUP
-#line 53 "lex.l"
-{ yylval = yytext; return TYPE; }
+#line 62 "lex.l"
+{ yylval = yytext[0]; return CHAR; }
 	YY_BREAK
 case 35:
 YY_RULE_SETUP
-#line 54 "lex.l"
-{ yylval = yytext; return ID; }
+#line 63 "lex.l"
+{ yylval = yytext; return TYPE; }
 	YY_BREAK
 case 36:
 YY_RULE_SETUP
-#line 56 "lex.l"
+#line 64 "lex.l"
+{ yylval = yytext; return ID; }
+	YY_BREAK
+case 37:
+YY_RULE_SETUP
+#line 66 "lex.l"
 {
   printf("Error type A at Line %d: Unknown characters \'%s\'\n", 
     yylineno, yytext);
 }
 	YY_BREAK
-case 37:
+case 38:
 YY_RULE_SETUP
-#line 61 "lex.l"
+#line 71 "lex.l"
 ECHO;
 	YY_BREAK
-#line 1010 "lex.yy.c"
+#line 1024 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -2023,7 +2037,7 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 61 "lex.l"
+#line 71 "lex.l"
 
 
 int main(int argc, char **argv) {
