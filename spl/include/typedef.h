543ab8413fb3b56042baf8d4fe93d50cc8795bbf
Alan Ai <aijunda29@gmail.com>
2020-11-17 18:52:02 +0800

    Partial commit

---
+++ spl/include/typedef.h
@@ -0,0 +1,33 @@
+#ifndef TYPEDEF_H
+#define TYPEDEF_H
+
+typedef struct Type {
+  char name[32];
+  enum {
+    PRIMITIVE,
+    ARRAY,
+    STRUCTURE
+  } category;
+  union {
+    enum {
+      INT,
+      FLOAT,
+      CHAR
+    } primitive;
+    struct Array *array;
+    struct FieldList *structure;
+  };
+} Type;
+
+typedef struct Array {
+  struct Type *base;
+  int size;
+} Array;
+
+typedef struct FieldList {
+  char name[32];
+  struct Type *type;
+  struct FieldList *next;
+} FieldList;
+
+#endif  // TYPEDEF_H
--- spl/src/C/flex/bison/syntax.y
+++ spl/src/C/flex/bison/syntax.y
@@ -3,7 +3,6 @@
   #include <iostream>
   #include <string>
   #include "include/astdef.h"
-  // #include "lex.yy.c"
 
   using namespace std;
 
