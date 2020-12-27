c8a7ef0f02af80ba65a4a587c47ed25728c788ec
Alan Ai <aijunda29@gmail.com>
2020-12-27 22:16:50 +0800

    Add splc script

---
+++ project3/splc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "-----------------------------------------"
+echo "SPLC running [$1] test case [$2]"
+echo "-----------------------------------------"
+
+if test $1 = "sample"; then
+    ./bin/splc ./sample/test0$2.spl
+elif test $1 = "test"; then
+    if test $2 -lt 10; then
+        ./bin/splc ./test/test_3_r0$2.spl
+    else
+        ./bin/splc ./test/test_3_r$2.spl
+    fi
+fi
