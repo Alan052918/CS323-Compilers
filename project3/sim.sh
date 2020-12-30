af0b1737a7d416b3735c7b7bc6249ed5af951856
Alan Ai <aijunda29@gmail.com>
2020-12-30 16:52:51 +0800

    Modify simulator script to support dual oses

--- project3/sim.sh
+++ project3/sim.sh
@@ -3,17 +3,30 @@ echo "---------------------------------------------"
 echo "SIMULATING running [$1] test case [$2]"
 echo "---------------------------------------------"
 
+OS=$(uname -a | cut -d" " -f 1)
+LINUX_IRSIM=./simulator/dist-Ubuntu/irsim
+MACOS_IRSIM=./simulator/dist-macOS/irsim
+
+if test $OS = "Darwin"; then
+  IRSIM=$MACOS_IRSIM
+elif test $OS = "Linux"; then
+  IRSIM=$LINUX_IRSIM
+else
+  echo "Unsupported operating system"
+  exit 1
+fi
+
 if test $1 = "test"; then
   if test $2 -lt 10; then
-    ./simulator/dist/irsim ./test/test_3_r0$2.ir
+    $IRSIM ./test/test_3_r0$2.ir
   else
-    ./simulator/dist/irsim ./test/test_3_r$2.ir
+    $IRSIM ./test/test_3_r$2.ir
   fi
 elif test $1 = "sample"; then
   if test $2 -lt 10; then
-    ./simulator/dist/irsim ./sample/test0$2.ir
+    $IRSIM ./sample/test0$2.ir
   else
-  echo "test case index out of bounds: sample only has 4 test cases"
+    echo "test case index out of bounds: sample only has 4 test cases"
   fi
 else
   echo "Unsupported directory: $1"
